e4cf634797ab6f01dcb9e952639ff6a6d33d6f99
Antonio Thomas Atkinson <Antonio01pd2014@atkinson-antonio-thomas-2.local>
2017-08-29 00:18:27 -0400

    Added ability to input addtional data from command line

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pprint
 import sys
+import time
 
 print(sys.argv[1])
 print(sys.argv[2])
@@ -17,16 +18,30 @@ sheet = client.open('database').sheet1
 pp = pprint.PrettyPrinter()
 # data = sheet.get_all_records()
 data = sheet.cell(1,1).value
+data = sheet.col_values(1)
 
 pp.pprint(data)
 
-#row=["Michigan","Is","Awesome"]
-# x = raw_input('What is your name?')
-# print x
+var = raw_input("Enter an entry to database ")
 
-row = [sys.argv[1],sys.argv[2],sys.argv[3]]
+#var= "testing this function"
+testVar= [] 
+testVar= var.split()
+
+print "Current date "  + time.strftime("%x")
+ 
+## Only time representation
+print "Current time " + time.strftime("%X")
+print (testVar[0]+","+testVar[1])
+#row=["Michigan","Is","Awesome"]
+#x = raw_input('Input row data')
+#print x[1]
+
+row = [sys.argv[1],sys.argv[2],sys.argv[3],time.strftime("%x"),time.strftime("%X")]
 
 sheet.insert_row(row,2)
 
-print(sheet.row_count)
+row = sheet.col_values(1)
+
+print(row)
 
